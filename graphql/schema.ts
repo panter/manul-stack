7e5ba8ad1313b061afd877d0459f27f76e813aaf
Stefan Zollinger <zos@panter.ch>
2020-09-18 15:04:44 +0200

    chore: use whitelisting for allowed queries & fields

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,7 +1,7 @@
-import { schema, use, settings, server } from "nexus";
+import { schema, use, settings } from "nexus";
 
 import { prisma } from "nexus-plugin-prisma";
-import { shield } from "nexus-plugin-shield";
+import { deny, shield } from "nexus-plugin-shield";
 import { auth } from "nexus-plugin-jwt-auth";
 
 import { APP_SECRET } from "./utils/user";
@@ -54,6 +54,8 @@ use(
     rules,
     options: {
       allowExternalErrors: true,
+      fallbackRule: deny,
+      debug: true,
     },
   })
 );
